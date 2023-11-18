358f885f1476ac43eeb4975e04fd8f392c7ba9d5
dipGhost <deepghosh@pandatoto.in>
2023-11-19 00:24:15 +0530

    added the app details

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@"f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+
+db = SQLAlchemy(app)
+api = Api(app)
+
